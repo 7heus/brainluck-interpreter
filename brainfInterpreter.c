f3eb8770216e10a4d88d26a5b0ddda4a59ee4d94
Theus <matheusgoias@hotmail.com>
2025-02-06 19:05:25 -0300

    Update brainfInterpreter.c

--- brainfInterpreter.c
+++ brainfInterpreter.c
@@ -206,3 +206,49 @@ EXPORT char *toBf(const char *text)
 
     return output;
 }
+
+static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+EXPORT char *b64_encode(const unsigned char *src, int *len)
+{
+    unsigned char *out, *pos;
+    const unsigned char *end, *in;
+
+    int olen = 4 * ((len + 2) / 3);
+
+    if (olen < len)
+        return NULL;
+    char *outstring = (char *)malloc(sizeof(char) * olen);
+    out = (unsigned char *)&outstring[0];
+
+    end = src + len;
+    in = src;
+    pos = out;
+
+    while (end - in >= 3)
+    {
+        *pos++ = base64_table[in[0] >> 2];
+        *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+        *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+        *pos++ = base64_table[in[2] & 0x3f];
+        in += 3;
+    }
+
+    if (end - in)
+    {
+        *pos++ = base64_table[in[0] >> 2];
+        if (end - in == 1)
+        {
+            *pos++ = base64_table[(in[0] & 0x03) << 4];
+            *pos++ = '=';
+        }
+        else
+        {
+            *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+            *pos++ = base64_table[(in[1] & 0x0f) << 2];
+        }
+        *pos++ = '=';
+    }
+
+    return outstring;
+}
