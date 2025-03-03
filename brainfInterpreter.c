8ca6431a878b0c47199abae2812f47111e80b49f
Matheus Almeida <matheusgoias@hotmail.com>
2025-03-03 22:29:38 +0100

    Update brainfInterpreter.c

--- brainfInterpreter.c
+++ brainfInterpreter.c
@@ -12,7 +12,6 @@
 
 EXPORT char *interpreter(const char *code, const char *input);
 EXPORT void free_memory(char *ptr);
-EXPORT char *toBf(const char *text);
 
 EXPORT char *interpreter(const char *code, const char *input)
 {
@@ -144,111 +143,4 @@ EXPORT char *interpreter(const char *code, const char *input)
 EXPORT void free_memory(char *ptr)
 {
     free(ptr);
-}
-
-EXPORT char *toBf(const char *text)
-{
-    int memIndex = 0;
-    short currentVal = 0;
-    char *output = (char *)malloc(1 * sizeof(char));
-    int output_index = 0;
-
-    for (size_t i = 0; i < strlen(text); i++)
-    {
-        int targetVal = text[i];
-        short diff = targetVal - currentVal;
-
-        if (diff != 0)
-        {
-            if (abs(diff) > 10)
-            {
-                int loopValue = abs(diff) / 10;
-                int remainder = abs(diff) % 10;
-
-                output = (char *)realloc(output, (output_index + 5) * sizeof(char));
-                output[output_index++] = '+';
-                output[output_index++] = '[';
-                output[output_index++] = '>';
-                for (int j = 0; j < loopValue; j++)
-                {
-                    output = (char *)realloc(output, (output_index + 2) * sizeof(char));
-                    output[output_index++] = '+';
-                }
-                output = (char *)realloc(output, (output_index + 3) * sizeof(char));
-                output[output_index++] = '<';
-                output[output_index++] = '-';
-                output[output_index++] = ']';
-
-                for (int j = 0; j < remainder; j++)
-                {
-                    output = (char *)realloc(output, (output_index + 2) * sizeof(char));
-                    output[output_index++] = (diff > 0) ? '+' : '-';
-                }
-            }
-            else
-            {
-                for (int j = 0; j < abs(diff); j++)
-                {
-                    output = (char *)realloc(output, (output_index + 2) * sizeof(char));
-                    output[output_index++] = (diff > 0) ? '+' : '-';
-                }
-            }
-        }
-
-        output = (char *)realloc(output, (output_index + 2) * sizeof(char));
-        output[output_index++] = '.';
-
-        currentVal = targetVal;
-    }
-
-    output = (char *)realloc(output, (output_index + 1) * sizeof(char));
-    output[output_index] = '\0';
-
-    return output;
-}
-
-static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-EXPORT char *b64_encode(const unsigned char *src, int *len)
-{
-    unsigned char *out, *pos;
-    const unsigned char *end, *in;
-
-    int olen = 4 * ((len + 2) / 3);
-
-    if (olen < len)
-        return NULL;
-    char *outstring = (char *)malloc(sizeof(char) * olen);
-    out = (unsigned char *)&outstring[0];
-
-    end = src + len;
-    in = src;
-    pos = out;
-
-    while (end - in >= 3)
-    {
-        *pos++ = base64_table[in[0] >> 2];
-        *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-        *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
-        *pos++ = base64_table[in[2] & 0x3f];
-        in += 3;
-    }
-
-    if (end - in)
-    {
-        *pos++ = base64_table[in[0] >> 2];
-        if (end - in == 1)
-        {
-            *pos++ = base64_table[(in[0] & 0x03) << 4];
-            *pos++ = '=';
-        }
-        else
-        {
-            *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-            *pos++ = base64_table[(in[1] & 0x0f) << 2];
-        }
-        *pos++ = '=';
-    }
-
-    return outstring;
 }
